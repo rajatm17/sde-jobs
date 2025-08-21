d144fdb4f143acec4721853e4d9959a4c4520664
rajatm17 <93270086+rajatm17@users.noreply.github.com>
2025-08-22 03:27:02 +0530

    Update Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,34 +3,34 @@ import { useState, useEffect } from "react";
 import Card from "./Card"
 
 
-export default function Cards(){
-    const [jobs,setJobs] = useState([{}]);
+export default function Cards() {
+  const [jobs, setJobs] = useState([{}]);
 
-
-useEffect(() => {
-  async function fetchData() {
-    try {
+  useEffect(() => {
+    async function fetchData() {
       const res = await fetch("/api/jobs");
       const data = await res.json();
-      console.log(data);
       setJobs(data);
-    } catch (err) {
-      console.error("Failed to fetch jobs", err);
     }
-  }
-  fetchData();
-}, []);
+    fetchData();
+  }, []);
 
-
-
-
-    return <>
-        <div class="grid grid-cols-1 place-items-center items-center justify-center md:grid-cols-2 gap-4">
-        {
-            jobs.map((job) =>(
-                <Card title={job.title} orgUrl={job.organization_url} location={job.locations_raw} url={job.url} org={job.organization} />
-            ))
-        }
-        </div>
+  return (
+    <>
+      <div className="grid grid-cols-1 place-items-center items-center justify-center md:grid-cols-2 gap-4">
+        {jobs.map((job, i) => (
+          <Card
+            key={i}
+            title={job.title}
+            orgUrl={job.organization_url}
+            location={job.locations_raw}
+            url={job.url}
+            org={job.organization}
+          />
+        ))}
+      </div>
     </>
+  );
+}
+
 
