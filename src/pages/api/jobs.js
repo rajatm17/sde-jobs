48f1c1b422bebb0bd3fd8ed2001c37c6bfee4865
rajat <mehtarajat1772002@gmail.com>
2025-08-25 19:27:35 +0530

    changed task it

--- src/pages/api/jobs.js
+++ src/pages/api/jobs.js
@@ -2,7 +2,7 @@
 export async function GET() {
   const apiToken = import.meta.env.API_TOKEN;
   const apiToken2 = import.meta.env.API_TOKEN2; // no PUBLIC_ prefix
-  const apiUrl = `https://api.apify.com/v2/actor-tasks/s3dtSTZSZWFtAVLn5/runs/last/dataset/items?token=${apiToken2}&format=json&status=SUCCEEDED`;
+  const apiUrl = `https://api.apify.com/v2/actor-tasks/Vh6MZ3EauonePlBg3/runs/last/dataset/items?token=${apiToken2}&format=json&status=SUCCEEDED`;
   const jobfeed = `gXYtSOztqDkDEkbPV`
   try {
     const res = await fetch(apiUrl);
