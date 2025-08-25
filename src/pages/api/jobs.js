c632f938fa99122b676c5a3f6701716e3ae5da21
rajat <mehtarajat1772002@gmail.com>
2025-08-25 16:43:33 +0530

    changed api

--- src/pages/api/jobs.js
+++ src/pages/api/jobs.js
@@ -1,8 +1,8 @@
 // src/pages/api/jobs.js
 export async function GET() {
   const apiToken = import.meta.env.API_TOKEN; // no PUBLIC_ prefix
-  const apiUrl = `https://api.apify.com/v2/actor-tasks/gXYtSOztqDkDEkbPV/runs/last/dataset/items?token=${apiToken}&format=json&status=SUCCEEDED`;
-
+  const apiUrl = `https://api.apify.com/v2/actor-tasks/s3dtSTZSZWFtAVLn5/runs/last/dataset/items?token=${apiToken}&format=json&status=SUCCEEDED`;
+  const jobfeed = `gXYtSOztqDkDEkbPV`
   try {
     const res = await fetch(apiUrl);
     const data = await res.json();
